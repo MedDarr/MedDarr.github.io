00cb2ab7350f546dfd965a3845401bfe30d85fb0
MedDarr <112845790+MedDarr@users.noreply.github.com>
2022-09-29 01:08:53 +0300

    Update style.js

--- style.js
+++ style.js
@@ -3,5 +3,5 @@ var url = "https://meddarr.github.io/3.html";
 if(urlTNP != url){ 
 setTimeout(function(){
   window.location.href = 'https://meddarr.github.io/3.html';
-}, 2 * 1000);
+}, 5 * 1000);
 }

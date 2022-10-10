cefa7d4d1a689b8b29e8cbe6602d64309ef2c07b
MedDarr <112845790+MedDarr@users.noreply.github.com>
2022-10-10 22:52:24 +0300

    Update script5.js

--- script5.js
+++ script5.js
@@ -4,9 +4,9 @@ function cost(){
     var num1=document.getElementById('price').value;
     var num2 =document.getElementById('quantity').value;
     let res=document.getElementById('result');
-    if( (isNaN(num1) || num1<0) && ( isNaN(num2)|| num2<0 || !(Number.isInteger(num2))))
+    if( (isNaN(num1) || num1<0) && ( isNaN(num2)|| num2<0 || (Number.isInteger(num2))))
     { res.innerHTML = "Введены неккоректные данные";}
-    else if( isNaN(num2) || num2<0 || !(Number.isInteger(num2)))  res.innerHTML = "Введено неверное количество товара";
+    else if( isNaN(num2) || num2<0 || (Number.isInteger(num2)))  res.innerHTML = "Введено неверное количество товара";
     else if(isNaN(num1) || num1<0)  res.innerHTML = "Введена неверная цена ";
     else{ var result=num1*num2;
         res.innerHTML ="Стоимость заказа: "+ result;

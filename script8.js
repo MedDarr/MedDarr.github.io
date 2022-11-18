be6e704fdbb94418b94548e74e8610675907192e
MedDarr <112845790+MedDarr@users.noreply.github.com>
2022-11-18 20:02:08 +0300

    Update script8.js

--- script8.js
+++ script8.js
@@ -9,3 +9,35 @@ function PopUpShow(){
 function PopUpHide(){
     $("#popup1").hide();
 }
+ function clear() {
+  let namefield = document.getElementsByName("name");
+  let email = document.getElementsByName("email");
+  let message = document.getElementsByName("message");
+  let check = document.getElementsByName("check");
+  namefield[0].value = "";
+  email[0].value = "";
+  message[0].value = "";
+  check[0].checked = false;
+}
+  $(function(){
+      $(".ajaxForm").submit(function(e){
+          e.preventDefault();
+          var href = $(this).attr("action");
+          $.ajax({
+              type: "POST",
+              dataType: "json",
+              url: href,
+              data: $(this).serialize(),
+              success: function(response){
+                  if(response.status == "success"){
+                     
+                      alert("Данные отправлены, спасибо!");
+                      clear();
+                      PopUpHide()
+                  }else{
+                      alert("Ошибка отправления: " + response.message);
+                  }
+              }
+          });
+      });
+  });
